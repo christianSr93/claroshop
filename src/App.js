828d01c372c6b9716cbadeaa52021f9da3a7c70b
christianSr93 <christian.sanchez.93@gmail.com>
2022-11-09 14:34:20 -0600

    feat: add Layout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import Layout from './Components/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Layout/>
     </div>
   );
 }
