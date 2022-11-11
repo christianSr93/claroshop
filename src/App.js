e931f6cd244d1e4d7b1d726c0a39cef3bae91a53
christianSr93 <christian.sanchez.93@gmail.com>
2022-11-11 13:17:38 -0600

    feat: add footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import Layout from './Components/Layout/Layout';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Layout/>
+      <Footer/>
     </div>
   );
 }
